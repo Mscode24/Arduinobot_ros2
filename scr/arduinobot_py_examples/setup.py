46959ba937ed0f76afff9152d87aa0b89f0d9ada
Mariselvam <mariselvamg25ra@gmail.com>
2025-02-04 15:06:56 +0530

    add launch file

--- scr/arduinobot_py_examples/setup.py
+++ scr/arduinobot_py_examples/setup.py
@@ -5,7 +5,7 @@ package_name = 'arduinobot_py_examples'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=find_packages(exclude=['tests']),  # Assuming 'tests' is the directory to exclude
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -14,14 +14,16 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Mariselvam',
-    maintainer_email='edsols@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer_email='your-email@example.com',  # Replace with actual email
+    description='A package with examples for ArduinoBot integration',
+    license='MIT',  # Replace with your actual license
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-            'simple_publisher = arduinobot_py_examples.simple_publisher:main',
-            'simple_subscriber = arduinobot_py_examples.simple_subscriber:main'
-        ],
-    },
+    'console_scripts': [
+        'simple_publisher = arduinobot_py_examples.simple_publisher:main',
+        'simple_subscriber = arduinobot_py_examples.simple_subscriber:main',
+        'simple_parameter = arduinobot_py_examples.simple_parameter:main',
+    ],
+}
+
 )
